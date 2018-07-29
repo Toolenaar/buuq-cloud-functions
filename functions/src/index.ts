2d0cffb7a73093a0806010794e36de8985201e8b
jochem toolenaar <toolenaar@gmail.com>
2018-07-29 22:17:47 +0200

    fix: removed hello world function

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -5,11 +5,6 @@ const db = admin.firestore();
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
-//
-export const helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Firebase!");
-});
-
 export const onTransactionWrite = functions.firestore.document('transactions/{id}').onWrite((change, context) => {
 
     // check if deleted
